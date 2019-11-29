b039f4736323c33f264807a880b482f2c94dcbeb
natiacostap <nati_aap@hotmail.com>
2019-11-29 20:59:47 -0300

    creamos primera vista hone

---
+++ src/Routes.js
@@ -0,0 +1,18 @@
+ 
+import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+import Home from './views/Home'
+
+
+
+const Routes = () =>{
+    return(
+        <Switch>
+            <Route exact path='/' component = {Home}/>  
+            <Route exact path='/Home' component = {Home}/>
+            
+
+        </Switch>
+    )
+}
+export default Routes;
---
+++ src/components/Buttons.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+
+class Home extends Component {
+    render(){
+        return(
+            <h1> Hola! </h1>
+        )
+    }
+}
+export default Home
